dd0f06f276d9b27fd2c826fd1034774595d908d1
fabiodaffa <fabiodaffa17@gmail.com>
2021-03-30 06:06:57 +0700

    add else statement

--- hello.rb
+++ hello.rb
@@ -4,11 +4,6 @@
 
 # Lesson 2: Prompting User Input
 
-# name = gets.chop
-# puts "Hey, #{name}! Nice to meet you!"
+ name = gets.chop
+ puts "Hey, #{name}! Nice to meet you!"
 
- if color == "blue"
-    puts "Wow! My favorite color is #{color} also!"
- else
-    puts "I'm not a big fan of #{color}."
- end
